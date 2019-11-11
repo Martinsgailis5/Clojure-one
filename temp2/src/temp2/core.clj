d17b9fb596209ead4c63ed866b8b67a327b2d380
BuildTools <unconfigured@null.spigotmc.org>
2019-11-11 17:46:39 +0200

    Clojure task 1

---
+++ temp2/src/temp2/core.clj
@@ -0,0 +1,17 @@
+(ns temp2.core)
+(defn clojure-one [vars] 
+
+  (float 
+    (/  
+      ( reduce + 
+               ( map (fn[x] (val x))
+                     ( filter (fn[k] (.endsWith (key k) "a")) vars )
+               )
+      )
+      
+      ( count
+        ( filter (fn[k] (.endsWith (key k) "a")) vars)
+      )
+    ) 
+  )
+)
